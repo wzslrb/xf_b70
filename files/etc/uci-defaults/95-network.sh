ddbfcef18e402ea9d7b38ff5a4f9b5c4695d387c
烂橙饶命Ⅱ <67636812+wzslrb@users.noreply.github.com>
2020-09-16 17:54:15 +0800

    Update and rename 97-network.sh to 95-network.sh

--- files/etc/uci-defaults/95-network.sh
+++ files/etc/uci-defaults/95-network.sh
@@ -35,4 +35,29 @@ uci -q batch <<-EOF >/dev/null
 	commit network
 EOF
 service network restart
+
+echo "初始化dhcp" >> /mnt/sda1/112.txt
+uci -q batch <<-EOF >/dev/null
+	set dhcp.lan=dhcp
+	set dhcp.lan.interface='lan'
+	set dhcp.lan.start='100'
+	set dhcp.lan.limit='150'
+	set dhcp.lan.leasetime='12h'
+	set dhcp.wan=dhcp
+	set dhcp.wan.ignore='1'
+	set dhcp.lcrm2=dhcp
+	set dhcp.lcrm2.start='100'
+	set dhcp.lcrm2.leasetime='12h'
+	set dhcp.lcrm2.limit='150'
+	set dhcp.lcrm2.interface='lcrm2'
+	commit dhcp
+EOF
+service dnsmasq restart
+
+echo "初始化防火墙wan2 lcrm2" >> /mnt/sda1/112.txt
+uci add_list firewall.@zone[0].network='lcrm2'
+uci add_list firewall.@zone[1].network='wan2'
+uci commit firewall
+service firewall reload
+
 exit 0
