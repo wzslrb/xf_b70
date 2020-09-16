3de0552c5200a0123cf17338ba582075191a2808
烂橙饶命Ⅱ <67636812+wzslrb@users.noreply.github.com>
2020-09-17 06:54:08 +0800

    Update 105-ftp-sqm-adg.sh

--- files/etc/uci-defaults/105-ftp-sqm-adg.sh
+++ files/etc/uci-defaults/105-ftp-sqm-adg.sh
@@ -5,7 +5,7 @@ echo "重设vsftpd" >> /mnt/sda1/112.txt
 uci set vsftpd.listen.pasv_min_port='50000'
 uci set vsftpd.listen.pasv_max_port='51000'
 uci commit vsftpd
-service vsftpd restart 2 >> /mnt/sda1/112.txt
+#service vsftpd restart 2 >> /mnt/sda1/112.txt
 }
 
 [ -f "/etc/config/sqm" ] && {
@@ -19,7 +19,7 @@ uci set sqm.@queue[0].qdisc='cake'
 uci set sqm.@queue[0].script='piece_of_cake.qos'
 uci set sqm.@queue[0].enabled='1'
 uci commit sqm
-service sqm restart
+#service sqm restart
 }
 [ -f "/etc/config/AdGuardHome" ] && {
 echo "重设AdGuardHome" >> /mnt/sda1/112.txt
@@ -29,8 +29,10 @@ uci set AdGuardHome.@AdGuardHome[0].enabled='1'
 uci set AdGuardHome.@AdGuardHome[0].waitonboot='1'
 uci set AdGuardHome.@AdGuardHome[0].redirect='redirect'
 uci set AdGuardHome.@AdGuardHome[0].configpath='/etc/config/AdG112.yaml'
+uci set AdGuardHome.AdGuardHome.enabled='0'
+uci set AdGuardHome.AdGuardHome.waitonboot='0'
 uci commit AdGuardHome
-service AdGuardHome stop
+#service AdGuardHome stop
 }
 
 [ -s "/usr/share/AdGuardHome/links.txt" ] && {
